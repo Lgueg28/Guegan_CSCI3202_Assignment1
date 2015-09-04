f1a3582a505a4f1fec32a8a4b10f0e0581af5e48
Lgueg28 <loic.guegan@colorado.edu>
2015-09-04 00:45:20 -0600

    Update Guegan_Assignment1.py

--- Guegan_Assignment1.py
+++ Guegan_Assignment1.py
@@ -1,21 +1,3 @@
-#Queue Import Module
-import Queue 
-
-q = Queue.Queue()
-
-#Stack Implementation
-class Stack:
-	def _init_(self):
-		self.items = []
-	
-	def push(self, number):
-		self.items.append(number)
-
-	def pop(self):
-		return self.items.pop()
-
-	def checkSize(self):
-		return len(self.items)
 #Loic Guegan Assignment 1 Submission
 #Stack is in stack.py
 #Queue is is queue.py
